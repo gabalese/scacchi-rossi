9ab1f6dc106106604da43945c102c5d9c61f3e62
Gabriele Alese <gabriele@letolab.com>
2015-02-06 15:43:41 +0000

    Refactoring: the contiguous position should be Move responsibility

--- src/main/scala/it/alese/scacchirossi/scacchirossi/Move.scala
+++ src/main/scala/it/alese/scacchirossi/scacchirossi/Move.scala
@@ -1,11 +1,50 @@
 package it.alese.scacchirossi.scacchirossi
 
+import it.alese.scacchirossi.scacchirossi.board.{Column, Row}
+
 case class Move(from: Position, to: Position) {
   require(ChessBoard.validPositions contains from)
   require(ChessBoard.validPositions contains to)
   require(from != to)
 
+  private val offset = Offset(to.x - from.x, to.y - from.y)
+
   def distance: (Int, Int) = {
-    (to.x - from.x, to.y - from.y)
+    (offset.horizontal, offset.vertical)
   }
+
+  def intermediatePositions: List[Position] = {
+    if (offset.horizontal == 0 || offset.vertical == 0) {
+      // straight line movement
+      fetchStraightPositions
+    } else if (offset.horizontal == offset.vertical) {
+      // diagonal movement
+      fetchDiagonalPositions
+    } else {
+      // L-shaped movement
+      fetchLshapedPositions
+    }
+  }
+
+  private def fetchStraightPositions: List[Position] = {
+    lazy val positions = from.x to to.x flatMap { col => from.y to to.y map { row => Position(Column(col), Row(row))}}
+    positions.toList
+  }
+
+  private def fetchDiagonalPositions: List[Position] = {
+    lazy val positions = (0 to offset.vertical by offset.vertical.signum) map { addendum => from +(addendum, addendum)}
+    positions.toList
+  }
+
+  private def fetchLshapedPositions: List[Position] = {
+    // L-shaped movement
+    // We assume that one moves the Knight vertically then horizontally
+    // the intermediate positions between a L-shaped movement are irrelevant, i.e. the Knight can jump over other pieces
+    lazy val horizontalPositions = ((0 to offset.vertical by offset.vertical.signum) map { addendum => from +(0, addendum)}).toList
+    lazy val verticalPositions = ((0 to offset.horizontal by offset.horizontal.signum) map { addendum => horizontalPositions.last +(addendum, 0)}).toList
+    horizontalPositions ++ verticalPositions.tail // verticalPosition.head == horizontalPosition.last
+  }
+
 }
+
+case class Offset(horizontal: Int, vertical: Int)
--- src/main/scala/it/alese/scacchirossi/scacchirossi/Position.scala
+++ src/main/scala/it/alese/scacchirossi/scacchirossi/Position.scala
@@ -1,38 +1,20 @@
 package it.alese.scacchirossi.scacchirossi
 
-import it.alese.scacchirossi.scacchirossi.board.{Row, Column}
+import it.alese.scacchirossi.scacchirossi.board.{Column, Row}
 
 case class Position(column: Column, row: Row) {
 
-  def x: Int = column.toInt
-  def y: Int = row.toInt
+  val x: Int = column.toInt
+
+  val y: Int = row.toInt
 
   def to(toPosition: Position): List[Position] = {
-
-    val horizontalOffset = toPosition.x - x
-    val verticalOffset = toPosition.y - y
-
-    if (horizontalOffset == 0 || verticalOffset == 0) {
-      // straight line movement
-      val positions = this.x to toPosition.x flatMap { col => this.y to toPosition.y map { row => Position(Column(col), Row(row))}}
-      positions.toList
-    } else if (horizontalOffset == verticalOffset) {
-      // diagonal movement
-      val positions = (0 to verticalOffset by verticalOffset.signum) map { addendum => this + (addendum, addendum) }
-      positions.toList
-    } else {
-      // L-shaped movement
-      // We assume that one moves the Knight vertically then horizontally
-      // the intermediate positions between a L-shaped movement are irrelevant, i.e. the Knight can jump over other pieces
-      val horizontalPositions = ((0 to verticalOffset by verticalOffset.signum) map { addendum => this +(0, addendum)}).toList
-      val verticalPositions = ((0 to horizontalOffset by horizontalOffset.signum) map { addendum => horizontalPositions.last + (addendum, 0)}).toList
-      horizontalPositions ++ verticalPositions.tail // verticalPosition.head == horizontalPosition.last
-      }
-    }
+    Move(this, toPosition).intermediatePositions
+  }
 
   def +(x: Int, y: Int) = {
-    val col = if(this.x + x != 0) Column(this.x + x) else this.column
-    val row = if(this.y + y != 0) Row(this.y + y) else this.row
+    val col = if (this.x + x != 0) Column(this.x + x) else this.column
+    val row = if (this.y + y != 0) Row(this.y + y) else this.row
     new Position(col, row)
   }
 
